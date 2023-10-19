71ad57b1e272beb4dd84611ff5303522d0263504
ashrithsubash <121907505+ashrithsubash@users.noreply.github.com>
2023-10-19 12:11:05 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,25 @@
+song1="";
+song2="";
+
+function preload()
+{
+    song = loadSound("song1.mp3");
+    song = loadSound("song2.mp3");
+}
+
+function setup()
+{
+    canvas = createCanvas(600,500);
+    canvas.center();
+
+    video = createCapture(VIDEO);
+    video.hide();
+
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses);
+}
+
+function draw()
+{
+    image(video, 0, 0, 600, 500);
+}
